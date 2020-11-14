bf80e9c8289a9db17128356006f2f567c534607f
juanFajardoUPTC <juanFajardoUPTC@github.com>
2020-11-14 16:25:03 -0500

    Tarea 2 lista

--- api.js
+++ api.js
@@ -3,36 +3,45 @@ const app = express()
 const port = 3000
 app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+let listaJuegos = ["DarkSouls", "CyberPunk", "Hades"]
+let listaPeliculas = ["Advengers", "300", "Interestelar"]
+let listaSeries = ["La casa de Papel", "Mr Robot", "One Punch Man"]
 
-//Ejemplo Params
-app.get('/juegos/:nombreJuego', function(req, res) {
+{   opcion: 'juegos'
+    opcion: 'peliculas'
+    opcion: 'series'
+}
+
+app.get('/', (req, res) => {
+    res.send('Pagina Principal!')
+  })
+
+app.get('/:opcion', function(req, res) {
 
     console.log()
-    if(req.params.nombreJuego === 'DarkSouls'){
-        res.send('Juego de Aventura')
+    if(req.params.opcion === 'juegos'){
+        listaJuegos.forEach(function(elemento, indice, array) {
+            console.log(elemento, indice);
+            res.send(elemento, indice)
+        })
+    }
+
+    if(req.params.opcion === 'peliculas'){
+        listaJuegos.forEach(function(elemento, indice, array) {
+            console.log(elemento, indice);
+            res.send(elemento, indice)
+        })
+    }
+
+    if(req.params.opcion === 'series'){
+        listaJuegos.forEach(function(elemento, indice, array) {
+            console.log(elemento, indice);
+            res.send(elemento, indice)
+        })
     }
   
 })
 
-{ nombreJuego: 'DarkSouls'}
-
-//Ejemplo Body
-app.post('/juegos/', function(req, res) {
-
-    console.log(req.body.name)
-    res.send('Pagina de juegos')
-   
-});
-
-//Ejemplo Query
-app.post('/peliculas/', function(req, res) {
-
-    console.log(req.query.name)
-    res.send('Pagina de Peliculas')
-});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
